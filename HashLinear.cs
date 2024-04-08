cd08c9410b7391e4e6e007513072af01dc11b1e1
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2024-04-08 10:33:29 -0300

    Implementa sondagem linear

--- HashLinear.cs
+++ HashLinear.cs
@@ -1,29 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace apCaminhosEmMarte
+public class HashLinear<T> : ITabelaDeHash<T>
+	where T : IRegistro<T>
 {
-  public class HashLinear<Tipo> : ITabelaDeHash<Tipo>
-    where Tipo : IRegistro<Tipo>
-  {
-    public List<Tipo> Conteudo()
-    {
-      throw new NotImplementedException();
-    }
+	private const int SIZE = 1001;
+	private T[] dados;
 
-    public bool Existe(Tipo item, out int onde)
-    {
-      throw new NotImplementedException();
-    }
+	public HashLinear()
+	{
+		dados = new T[SIZE];
+	}
 
-    public void Inserir(Tipo item)
-    {
-      throw new NotImplementedException();
-    }
+	public int Hash(string chave)
+	{
+		long hash = 0;
+		foreach (char c in chave)
+			hash += 37 * hash + c;
 
-    public bool Remover(Tipo item)
-    {
-      throw new NotImplementedException();
-    }
-  }
+		hash %= chave.Length;
+		if (hash < 0)
+			hash += chave.Length;
+		return (int)hash;
+	}
+
+	public void Inserir(T item)
+	{
+		int pos = Hash(item.Chave);
+		int initial_pos = pos;
+		bool hasReachedEnd = false;
+		while (Existe(item, out pos))
+		{
+			if (pos == dados.Length)
+			{
+				pos = 0;
+				hasReachedEnd = true;			
+			} else if (pos == initial_pos && hasReachedEnd == true) return;
+			else pos++;
+		}
+		dados[pos] = item;
+	}
+
+	public bool Remover(T item)
+	{
+		int pos;
+		if (Existe(item, out pos))
+		{
+			dados[pos] = default;
+			return true;
+		}
+		return false;
+	}
+
+	public bool Existe(T item, out int pos)
+	{
+		pos = Hash(item.Chave);
+		if (dados[pos] != null) return true;
+		else return false;
+	}
+
+	public List<T> Conteudo()
+	{
+		List<T> saida = new List<T>();
+		foreach (T item in dados)
+			if (item != null) saida.Add(item);
+		return saida;
+	}
 }
