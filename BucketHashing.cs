0edbac217312068d510edc3b9f1f0c579a2a6875
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2024-04-14 16:39:52 -0300

    Documenta implementação do bucket hashing

--- BucketHashing.cs
+++ BucketHashing.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections;
+﻿/*
+ * 23513 - Diogo Lourenço
+ * 23521 - Gustavo Cruz
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 
+// Implements a hash table with ArrayLists as hash buckets
 class BucketHashing<T> : IHashTable<T>
   where T : IRegistry<T>
 {
@@ -9,6 +15,7 @@ class BucketHashing<T> : IHashTable<T>
 
     public BucketHashing()
     {
+        // Instantiates the hash table
         data = new ArrayList[SIZE];
         for (int i = 0; i < SIZE; i++)
             data[i] = new ArrayList(1);
@@ -16,15 +23,19 @@ class BucketHashing<T> : IHashTable<T>
 
     public int Hash(string key)
     {
+        // Calulcates a hash for a key based on Horner's method
         long hash = 0;
         foreach (char c in key)
             hash += 37 * hash + c;
-        hash = hash % data.Length;
+        hash %= data.Length;
+        if (hash < 0)
+            hash += data.Length;
         return (int)hash;
     }
 
     public void Insert(T item)
     {
+        // Insert an item if not already
         int itemHashValue = Hash(item.Key);
         if (!data[itemHashValue].Contains(item))
             data[itemHashValue].Add(item);
@@ -32,6 +43,7 @@ class BucketHashing<T> : IHashTable<T>
 
     public bool Remove(T item)
     {
+        // Removes an item if found
         int itemPosition = 0;
         if (!Exists(item, out itemPosition))
             return false;
@@ -41,12 +53,14 @@ class BucketHashing<T> : IHashTable<T>
 
     public bool Exists(T item, out int itemPosition)
     {
+        // Checks whether an item exists
         itemPosition = Hash(item.Key);
         return data[itemPosition].Contains(item);
     }
 
     public List<T> Content()
     {
+        // Lists the contents for each bucket that has at least one value
         List<T> content = new List<T>();
         for (int i = 0; i < data.Length; i++)
             if (data[i].Count > 0)
