f9d0603838ee69ee982d3623f04c4073fd9aafa0
Gustavo Cruz <79774754+gstvcruz@users.noreply.github.com>
2024-04-15 18:12:07 -0300

    Implementa sondagem quadrática

--- QuadraticProbing.cs
+++ QuadraticProbing.cs
@@ -1,26 +1,77 @@
-﻿using System;
+/*
+ * 23513 - Diogo Lourenço
+ * 23521 - Gustavo Cruz
+*/
+
+using System;
 using System.Collections.Generic;
 
+// Implements quadratic probing
 public class QuadraticProbing<T> : IHashTable<T>
-  where T : IRegistry<T>
+    where T : IRegistry<T>
 {
-  public List<T> Content()
-  {
-    throw new NotImplementedException();
-  }
+    private const int SIZE = 131;
+    private T[] data;
 
-  public bool Exists(T item, out int itemPosition)
-  {
-    throw new NotImplementedException();
-  }
+    public QuadraticProbing()
+    {
+        data = new T[SIZE];
+    }
 
-  public void Insert(T item)
-  {
-    throw new NotImplementedException();
-  }
+    public int Hash(string key)
+    {
+        long hash = 0;
+        foreach (char c in key)
+            hash += 37 * hash + c;
+        hash %= SIZE;
+        if (hash < 0)
+            hash += SIZE;
 
-  public bool Remove(T item)
-  {
-    throw new NotImplementedException();
-  }
+        int initialPosition = (int)hash;
+        if ((data[initialPosition] == null) || (Content().Count >= SIZE / 2))
+            return initialPosition;
+
+        double currentPosition = initialPosition;
+        double collisions = 1;
+        // Loop over hash table positions looking for an empty spot
+        while (data[(int)currentPosition] != null)
+        {
+            // Keep looking for an empty spot
+            currentPosition = (currentPosition + Math.Pow(currentPosition, collisions++)) % SIZE;
+            if (currentPosition % SIZE >= SIZE)
+                return initialPosition;
+        }
+        return (int)currentPosition;
+    }
+
+    public void Insert(T item)
+    {
+        int itemHashValue;
+        if (!Exists(item, out itemHashValue))
+            data[itemHashValue] = item;
+    }
+
+    public bool Remove(T item)
+    {
+        int itemPosition;
+        if (!Exists(item, out itemPosition))
+            return false;
+        data[itemPosition] = default;
+        return true;
+    }
+
+    public bool Exists(T item, out int itemPosition)
+    {
+        itemPosition = Hash(item.Key);
+        return data[itemPosition] != null;
+    }
+
+    public List<T> Content()
+    {
+        List<T> content = new List<T>();
+        foreach (T item in data)
+            if (item != null)
+                content.Add(item);
+        return content;
+    }  
 }
